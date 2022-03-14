68fb9e3268438d6187dc7d32390b9416b0cb69de
nataliefrick <natalie.salomons@gmail.com>
2022-03-14 21:29:13 +0100

    readme

--- incl/navbar.php
+++ incl/navbar.php
@@ -3,8 +3,7 @@
         
         <li><a class="noline" href="login.php">Login</a></li>
         <li><a class="noline" href="all-recipes.php">Recipes</a></li>
-        <li><a class="noline" href="all-recipes.php">Articles</a></li>
-        <li><a class="noline" href="all-recipes.php">Book</a></li>
+        <li><a class="noline" href="privacy.php">Privacy Policy</a></li>
         <li><a class="noline" href="all-recipes.php">About</a></li>
 
     </ul>
---
+++ privacy.php
@@ -0,0 +1,90 @@
+<?php 
+include_once("incl/config.php"); 
+$page_title = "Home";
+include("incl/header.php"); 
+?>
+<h1 class="dont-show">Sweets Privacy Policy and GDPR compliancy</h1></p>
+
+<div class="row">
+    <section class="container" id="privacy">
+        <h2>Sweets Privacy Policy and GDPR compliancy</h2>
+        <p>This privacy policy will explain how Sweets uses the personal data we collect from you when you use our website.</p>
+
+        <h3>Personal identification information</h3> 
+        <p>(Name, email address, phone number, etc.)</p>
+
+        <h4>How do we collect your data?</h4>
+        <p>You directly provide Our Company with most of the data we collect. We collect data and process data when you:</p>
+        <ul>
+            <li>Register online or place an order for any of our products or services.</li>
+            <li>Voluntarily complete a customer survey or provide feedback on any of our message boards or via email.</li>
+            <li>Use or view our website via your browser’s cookies.</li>
+        </ul>
+
+        <h4>How will we use your data?</h4>
+        <p>Our Company collects your data so that we can:
+        <ul>
+            <li>Process your order and manage your account.</li>
+            <li>Email you with special offers on other products and services we think you might like.</li>
+        </ul>
+        <h4>How do we store your data?</h4>
+        <p>Our Company securely stores your data at [enter the location and describe security precautions taken].</p>
+
+        <p>Our Company will keep your [enter type of data] for [enter time period]. Once this time period has expired, we will delete your data by [enter how you delete users’ data].</p>
+
+        <h4>Marketing</h4>
+        <p>Our Company would like to send you information about products and services of ours that we think you might like, as well as those of our partner companies. If you have agreed to receive marketing, you may always opt out at a later date.</p>
+
+        <p>You have the right at any time to stop Sweets from contacting you for marketing purposes or giving your data to other members of the Sweets. If you no longer wish to be contacted for marketing purposes, please contact us.</p>
+
+        <h4>What are your data protection rights?</h4>
+        <p>Our Company would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+        <ul>
+            <li>The right to access – You have the right to request Our Company for copies of your personal data. We may charge you a small fee for this service.</li>
+
+            <li>The right to rectification – You have the right to request that Our Company correct any information you believe is inaccurate. You also have the right to request Our Company to complete the information you believe is incomplete.</li>
+
+            <li>The right to erasure – You have the right to request that Our Company erase your personal data, under certain conditions.</li>
+
+            <li>The right to restrict processing – You have the right to request that Our Company restrict the processing of your personal data, under certain conditions.</li>
+
+            <li>The right to object to processing – You have the right to object to Our Company’s processing of your personal data, under certain conditions.</li>
+
+            <li>The right to data portability – You have the right to request that Our Company transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+        </ul>
+        <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+
+        <h3>Cookies</h3>
+        <p>Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology.</p>
+
+        <p>For further information, visit allaboutcookies.org.</p>
+
+        <h4>How do we use cookies?</h4>
+        <p>Our Company uses cookies in a range of ways to improve your experience on our website, including:</p>
+        <ul>
+            <li>Keeping you signed in</li>
+            <li>Understanding how you use our website</li>
+        </ul>
+        <p>What types of cookies do we use? There are a number of different types of cookies, however, our website uses:</p>
+        <ul>
+            <li>Functionality – Our Company uses these cookies so that we recognize you on our website and remember your previously selected preferences. These could include what language you prefer and location you are in. A mix of first-party and third-party cookies are used.</li>
+            <li>Advertising – Our Company uses these cookies to collect information about your visit to our website, the content you viewed, the links you followed and information about your browser, device, and your IP address. Our Company sometimes shares some limited aspects of this data with third parties for advertising purposes. We may also share online data collected through cookies with our advertising partners. This means that when you visit another website, you may be shown advertising based on your browsing patterns on our website.</li>
+        </ul>
+        <h4>How to manage cookies</h4>
+        <p>You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.</p>
+
+        <h4>Privacy policies of other websites</h4>
+        <p>The Our Company website contains links to other websites. Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.</p>
+
+        <h4>Changes to our privacy policy</h4>
+        <p>Our Company keeps its privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 15 March 2022.</p>
+
+        <h4>How to contact us</h4>
+        <p>If you have any questions about Our Company’s privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.</p>
+
+        <h4>How to contact the appropriate authority</h4>
+        <p>Should you wish to report a complaint or if you feel that Our Company has not addressed your concern in a satisfactory manner, you may contact the Information Commissioner’s Office.</p>
+    </section>
+</div>
+<?php 
+include_once("incl/footer-index.php"); 
