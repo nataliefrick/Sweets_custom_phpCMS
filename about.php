d09b4c404dc27de860ec00812b1ceee2c92186bd
nataliefrick <natalie.salomons@gmail.com>
2022-03-16 10:28:34 +0100

    about page completed

--- about.php
+++ about.php
@@ -4,35 +4,32 @@ $page_title = "about";
 include("incl/header.php"); ?>
 
 <h1 class="dont-show">About the site</h1>
-
-<section class="container" id="about">
-    <h2>Our Story</h2>
-    <p>Browse and choose a recipe from our database of recipes. Any of these recipes will be sure to excite you taste palate and give your family or guests something to rave about!</p>
-</section>
-<section id="filter">
-    <h2 class="dont-show">Filter</h2>
-    <p>Filter recipes by author:</p>
-    <div id="filter-btns">
-        <a class="filter-btn" href="all-recipes.php">All</a>
-        <?php foreach($authors as $a) { ?> 
-            <a class="filter-btn" href="all-recipes.php?id=<?= $a['id'] ?>"><?= $a['name']; ?></a>
-        <?php } ?>
-    </div>
-</section>
-<section class="container" id="allRecipes">
-    <h2 class="dont-show">All Recipes</h2>
-    <?php foreach($recipes as $r) { ?>    
-        <article class="recipe-card">
-            <a href="show-recipe.php?id=<?= $r['id']; ?>">
-                <img src="img/<?= $r['imgLink']; ?>" alt="<?= $r['imgAlt']; ?>"></a>
-                <h2><?= $r['title']; ?></h2> 
-                <?= $recipe->truncateText($r['story'], 150); ?>
-                <a class="read-more u-link" href="show-recipe.php?id=<?= $r['id']; ?>">See Recipe</a>
+<div class="row">
+    <section class="container" id="our-story">
+        <h2 class="dont-show">Our Story</h2>
+        <picture>
+            <source type="image/webp" srcset="img/our-story.webp">
+            <img src="img/our-story.jpg" alt="profile picture of authors Natalie & Marie-France">
+        </picture>
+        <article>
+            <h2>Our Story</h2>
+            <p>Natalie and Mare-France met in 2010 when they both worked at the university in Östersund. Both were expats, both had Canadian roots, and both had husbands who were either not at home during the week or away for months for military service abroad. And so began the weekly dinners. Every week, they would meed for a simple dinner: to chat, eat delicious food and be in good company. It soon became obvious that, regardless of who was the hostess, the main course was becoming increasingly secondary and turning into something they had to get through before they could get to the pi&egrave;ce de r&eacute;sistance: <em>dessert</em>. Every week, the excitement around what the other had prepared for dessert grew and, probably because both have a sweet tooth, dessert became what dinner wase built around.</p>
         </article>
-    <?php } ?>
+    </section>
+</div>
+<div class="row" id="about-website">
+    <section class="container" >
+    <h2>About the website</h2>
+    <p>This website is the product of the final project for the course Web development 2, Data technique: PHP and Databases.</p>
 
+    <p>The project aims to create a dynamic website with a database containing source information, in other words a simple CMS. The website is to be designed considering the chosen audience, graphic profile, a logical structure and navigation, a combination of code including SQL and PHP to create a dynamic website based on an objectoriented design structure. </p>
 
-</section>
+    <p>Specifically this website is a blog portal which specializes in dessert recipes. Users can register and log in to write recipes which are saved to the database and published to the site.</p>
+    
+    </section>
+
+</div>
+
 
 
 
--- incl/navbar.php
+++ incl/navbar.php
@@ -4,7 +4,7 @@
         <li><a class="noline" href="login.php">Login</a></li>
         <li><a class="noline" href="all-recipes.php">Recipes</a></li>
         <li><a class="noline" href="privacy.php">Privacy Policy</a></li>
-        <li><a class="noline" href="all-recipes.php">About</a></li>
+        <li><a class="noline" href="about.php">About</a></li>
 
     </ul>
 </nav>
